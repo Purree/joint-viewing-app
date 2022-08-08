5a4b691ef5ba1ed0c982b15db53adc5a1a3b6420
Pure <kirill2283371@mail.ru>
2022-08-08 19:08:01 +0300

    Configure laravel websockets. Move Room controller to route groupe.

--- routes/room.php
+++ routes/room.php
@@ -2,14 +2,14 @@
 
 use App\Http\Controllers\RoomController;
 
-Route::name('rooms.')->prefix('rooms')->group(static function () {
-    Route::get('/', [RoomController::class, 'index'])->name('index');
-    Route::post('/', [RoomController::class, 'create'])->name('create');
+Route::name('rooms.')->prefix('rooms')->controller(RoomController::class)->group(static function () {
+    Route::get('/', 'index')->name('index');
+    Route::post('/', 'create')->name('create');
 
     Route::middleware('can:interact,room')->prefix('{room}')->group(static function () {
-        Route::delete('/', [RoomController::class, 'destroy'])->middleware('can:delete,room')->name('destroy');
-        Route::put('/', [RoomController::class, 'update'])->middleware('can:update,room')->name('update');
-        Route::post('/leave', [RoomController::class, 'leave'])->name('leave');
-        Route::post('/kick/{user}', [RoomController::class, 'kick'])->middleware('can:kick,room')->name('kick');
+        Route::delete('/', 'destroy')->middleware('can:delete,room')->name('destroy');
+        Route::put('/', 'update')->middleware('can:update,room')->name('update');
+        Route::post('/leave', 'leave')->name('leave');
+        Route::post('/kick/{user}', 'kick')->middleware('can:kick,room')->name('kick');
     });
 });
