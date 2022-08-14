e26374c41f432c5be2b1294523a5ca4f36bee1da
Pure <kirill2283371@mail.ru>
2022-08-14 03:48:19 +0300

    Fix opening private rooms by clicking on links in rooms list. Add redirect user from room if he isn't a member of it or if room isn't exists. Replace "No query results for model" error text with "Data not found"ю

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,7 +2,12 @@
 
 namespace App\Exceptions;
 
+use App\Services\Results\ResponseResult;
+use Exception;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -43,8 +48,16 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
-        });
+    }
+
+    public function render(
+        $request,
+        Exception|Throwable $exception
+    ): Response|JsonResponse|\Symfony\Component\HttpFoundation\Response {
+        if ($exception instanceof ModelNotFoundException) {
+            return ResponseResult::error('Data not found.', Response::HTTP_NOT_FOUND)->error;
+        }
+
+        return parent::render($request, $exception);
     }
 }
--- resources/js/views/Room/Index.vue
+++ resources/js/views/Room/Index.vue
@@ -5,6 +5,8 @@
 <script>
 import {API_GET_ROOM_DATA_BY_LINK_URL} from "@/api/rooms";
 import replaceDataInUri from "@/mixins/replaceDataInUri";
+import errorsHelper from "@/mixins/errors.js";
+import {mapState} from "vuex";
 
 export default {
     name: "Index",
@@ -12,18 +14,40 @@ export default {
         return {
             room: {
                 'id': 0,
-            }
+            },
+            users: [],
         }
     },
     async mounted() {
-        await axios.get(replaceDataInUri(API_GET_ROOM_DATA_BY_LINK_URL, {'roomLink': this.$route.params.link}))
-            .then((response) => {
-                this.room = response.data;
-            }).catch((error) => {
-                console.log(error);
-            });
+        try {
+            await axios.get(replaceDataInUri(API_GET_ROOM_DATA_BY_LINK_URL, {'roomLink': this.$route.params.link}))
+                .then((response) => {
+                    this.room = response.data;
+                })
+        } catch (error) {
+            errorsHelper.methods.openResponseNotification(error)
 
-        Echo.join(`room.${this.room.id}`);
+            if (error.response.status === 404) {
+                return this.$router.push('/404');
+            }
+        }
+
+        if (this.current_room?.id !== this.room.id) {
+            errorsHelper.methods.openNotification('You are not in this room');
+
+            if (window.history.length > 2) {
+                this.$router.back()
+            } else {
+                this.$router.push({'name': 'Rooms'})
+            }
+        }
+
+        Echo.join(`room.${this.room.id}`).here((users) => {
+            this.users = users;
+        });
+    },
+    computed: {
+        ...mapState('rooms', ['current_room']),
     }
 }
 </script>
