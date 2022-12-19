e016db6be4b24e2c72c40dd0718b783f6eedcf17
Pure <kirill2283371@mail.ru>
2022-12-19 21:13:21 +0300

    Fix message about unsupported POST method on password recovery with secret key.

--- resources/js/api/auth.js
+++ resources/js/api/auth.js
@@ -3,6 +3,6 @@ import {API} from "@/api/api_prefixes.js"
 export const API_LOGIN_URL = ['post', API + 'login'];
 export const API_REGISTRATION_URL = ['post', API + 'users'];
 export const API_LOGOUT_URL = ['post', API + 'logout'];
-export const API_PASSWORD_RECOVERY_URL = ['post', API + 'users/password'];
+export const API_PASSWORD_RECOVERY_URL = ['put', API + 'users/password'];
 
 
